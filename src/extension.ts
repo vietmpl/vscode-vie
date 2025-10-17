vietmpl/vscode-vie

commit 1cd5c304d0db5f5942c856c562deb72f4025c8ad

feat(parser): add edits provider

src/extension.ts
@@ -45,16 +45,57 @@ export async function getHighlightsScm(): Promise<string> {
 }
 
 class VieSemanticTokensProvider implements vsc.DocumentSemanticTokensProvider {
-	public constructor(
+	constructor(
 		private readonly parser: Parser,
 		private readonly language: Language,
 		private readonly legend: vsc.SemanticTokensLegend,
-		private readonly higlights: string,
+		private readonly highlights: string,
 	) {}
+
+	private lastTokens?: vsc.SemanticTokens;
+	private lastVersion?: number;
 
 	async provideDocumentSemanticTokens(
 		document: vsc.TextDocument,
-		_: vsc.CancellationToken,
+	): Promise<vsc.SemanticTokens> {
+		const tokens = await this.computeTokens(document);
+		this.lastTokens = tokens;
+		this.lastVersion = document.version;
+
+		return new vsc.SemanticTokens(tokens.data, `ver-${document.version}`);
+	}
+
+	async provideDocumentSemanticTokensEdits(
+		document: vsc.TextDocument,
+	): Promise<vsc.SemanticTokensEdits | vsc.SemanticTokens> {
+		const prevVersion = this.lastVersion;
+		const prevTokens = this.lastTokens;
+
+		if (!prevTokens || !prevVersion || document.version !== prevVersion + 1) {
+			const tokens = await this.computeTokens(document);
+			this.lastTokens = tokens;
+			this.lastVersion = document.version;
+
+			return new vsc.SemanticTokens(tokens.data, `ver-${document.version}`);
+		}
+
+		const newTokens = await this.computeTokens(document);
+
+		if (prevTokens.data.length !== newTokens.data.length) {
+			this.lastTokens = newTokens;
+			this.lastVersion = document.version;
+
+			return new vsc.SemanticTokens(newTokens.data, `ver-${document.version}`);
+		}
+
+		return {
+			edits: [],
+			resultId: `ver-${document.version}`,
+		};
+	}
+
+	private async computeTokens(
+		document: vsc.TextDocument,
 	): Promise<vsc.SemanticTokens> {
 		const builder = new vsc.SemanticTokensBuilder(this.legend);
 		const tree = this.parser.parse(document.getText());
@@ -63,15 +104,12 @@ class VieSemanticTokensProvider implements vsc.DocumentSemanticTokensProvider {
 			throw new Error("Tree parse error");
 		}
 
-		const query = new Query(this.language, this.higlights);
+		const query = new Query(this.language, this.highlights);
 		const captures = query.captures(tree.rootNode);
 
 		for (const { name, node } of captures) {
 			const tokenTypeIndex = this.legend.tokenTypes.indexOf(name);
-
-			if (tokenTypeIndex === -1) {
-				continue;
-			}
+			if (tokenTypeIndex === -1) continue;
 
 			builder.push(
 				node.startPosition.row,

commit 4638772b2073acd8dbac48643c5dbf776b4264ef

revert: feat(parser): add edits provider
This reverts commit 1cd5c304d0db5f5942c856c562deb72f4025c8ad
which introduced a data race.

src/extension.ts
@@ -45,57 +45,16 @@ export async function getHighlightsScm(): Promise<string> {
 }
 
 class VieSemanticTokensProvider implements vsc.DocumentSemanticTokensProvider {
-	constructor(
+	public constructor(
 		private readonly parser: Parser,
 		private readonly language: Language,
 		private readonly legend: vsc.SemanticTokensLegend,
-		private readonly highlights: string,
+		private readonly higlights: string,
 	) {}
-
-	private lastTokens?: vsc.SemanticTokens;
-	private lastVersion?: number;
 
 	async provideDocumentSemanticTokens(
 		document: vsc.TextDocument,
-	): Promise<vsc.SemanticTokens> {
-		const tokens = await this.computeTokens(document);
-		this.lastTokens = tokens;
-		this.lastVersion = document.version;
-
-		return new vsc.SemanticTokens(tokens.data, `ver-${document.version}`);
-	}
-
-	async provideDocumentSemanticTokensEdits(
-		document: vsc.TextDocument,
-	): Promise<vsc.SemanticTokensEdits | vsc.SemanticTokens> {
-		const prevVersion = this.lastVersion;
-		const prevTokens = this.lastTokens;
-
-		if (!prevTokens || !prevVersion || document.version !== prevVersion + 1) {
-			const tokens = await this.computeTokens(document);
-			this.lastTokens = tokens;
-			this.lastVersion = document.version;
-
-			return new vsc.SemanticTokens(tokens.data, `ver-${document.version}`);
-		}
-
-		const newTokens = await this.computeTokens(document);
-
-		if (prevTokens.data.length !== newTokens.data.length) {
-			this.lastTokens = newTokens;
-			this.lastVersion = document.version;
-
-			return new vsc.SemanticTokens(newTokens.data, `ver-${document.version}`);
-		}
-
-		return {
-			edits: [],
-			resultId: `ver-${document.version}`,
-		};
-	}
-
-	private async computeTokens(
-		document: vsc.TextDocument,
+		_: vsc.CancellationToken,
 	): Promise<vsc.SemanticTokens> {
 		const builder = new vsc.SemanticTokensBuilder(this.legend);
 		const tree = this.parser.parse(document.getText());
@@ -104,12 +63,15 @@ class VieSemanticTokensProvider implements vsc.DocumentSemanticTokensProvider {
 			throw new Error("Tree parse error");
 		}
 
-		const query = new Query(this.language, this.highlights);
+		const query = new Query(this.language, this.higlights);
 		const captures = query.captures(tree.rootNode);
 
 		for (const { name, node } of captures) {
 			const tokenTypeIndex = this.legend.tokenTypes.indexOf(name);
-			if (tokenTypeIndex === -1) continue;
+
+			if (tokenTypeIndex === -1) {
+				continue;
+			}
 
 			builder.push(
 				node.startPosition.row,