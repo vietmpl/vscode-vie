bf1e136b179a5d0b2b63ee96daea2eecfe0d76db
skewb1k <skewb1kunix@gmail.com>
2025-10-17 17:14:17 +0300

    perf: preload query and legend in global context

--- src/extension.ts
+++ src/extension.ts
@@ -1,3 +1,4 @@
+import { readFile } from "node:fs/promises";
 import { dirname, join } from "node:path";
 import { fileURLToPath } from "node:url";
 import * as vsc from "vscode";
@@ -6,71 +7,82 @@ import { Language, Parser, Query } from "web-tree-sitter";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-export async function initVieLanguage(): Promise<{
-	parser: Parser;
-	language: Language;
-}> {
-	await Parser.init();
-	const parser = new Parser();
-	const language = await Language.load(join(__dirname, "tree-sitter-vie.wasm"));
-	parser.setLanguage(language);
+// Global extension context.
+// Must be initialized with initContext() during extension activation
+// before any other code accesses it. Must not be modified after.
+let _globalContext: {
+	vieLanguage: Language;
+	highlightsQuery: Query;
+	legend: vsc.SemanticTokensLegend;
+} | null = null;
 
-	return { parser, language };
+// Initializes the global extension context.
+// Should be called once during extension activation.
+export async function initContext() {
+	if (!_globalContext) {
+		await Parser.init();
+
+		const vieLanguage = await Language.load(
+			join(__dirname, "tree-sitter-vie.wasm"),
+		);
+		const highlightsScm = await readFile(
+			join(__dirname, "highlights.scm"),
+			"utf8",
+		);
+		const highlightsQuery = new Query(vieLanguage, highlightsScm);
+
+		// TODO(skewb1k): Avoid hardcoding tokens here. Extract them from highlightsQuery instead.
+		const tokenTypes = [
+			"keyword",
+			"string",
+			"string.escape",
+			"variable",
+			"boolean",
+			"operator",
+			"punctuation",
+			"comment",
+			"function.call",
+			"punctuation.bracket",
+			"punctuation.delimiter",
+		];
+		// TODO(skewb1k): currently only tokenTypes are used. Consider adding tokenModifiers.
+		const legend = new vsc.SemanticTokensLegend(tokenTypes);
+
+		_globalContext = { vieLanguage, highlightsQuery, legend };
+	}
+	return _globalContext;
 }
 
-async function initLegend(): Promise<vsc.SemanticTokensLegend> {
-	const tokenTypes = [
-		"keyword",
-		"string",
-		"string.escape",
-		"variable",
-		"boolean",
-		"operator",
-		"punctuation",
-		"comment",
-		"function.call",
-		"punctuation.bracket",
-		"punctuation.delimiter",
-	];
-	const legend = new vsc.SemanticTokensLegend(tokenTypes);
-
-	return legend;
-}
-
-export async function getHighlightsScm(): Promise<string> {
-	const uri = vsc.Uri.file(join(__dirname, "highlights.scm"));
-	const buf = await vsc.workspace.fs.readFile(uri);
-
-	return buf.toString();
+// Returns the global extension context.
+// Throws an error if the context has not been initialized.
+export function getContext() {
+	if (!_globalContext)
+		throw new Error("Global context accessed before initialization.");
+	return _globalContext;
 }
 
 class VieSemanticTokensProvider implements vsc.DocumentSemanticTokensProvider {
-	public constructor(
-		private readonly parser: Parser,
-		private readonly language: Language,
-		private readonly legend: vsc.SemanticTokensLegend,
-		private readonly higlights: string,
-	) {}
+	public constructor(private readonly parser: Parser) {}
 
 	async provideDocumentSemanticTokens(
 		document: vsc.TextDocument,
 		_: vsc.CancellationToken,
 	): Promise<vsc.SemanticTokens> {
-		const builder = new vsc.SemanticTokensBuilder(this.legend);
-		const tree = this.parser.parse(document.getText());
+		const ctx = getContext();
 
+		const builder = new vsc.SemanticTokensBuilder(ctx.legend);
+		const tree = this.parser.parse(document.getText());
 		if (!tree) {
-			throw new Error("Tree parse error");
+			throw new Error("Parse error.");
 		}
 
-		const query = new Query(this.language, this.higlights);
-		const captures = query.captures(tree.rootNode);
-
+		const captures = ctx.highlightsQuery.captures(tree.rootNode);
 		for (const { name, node } of captures) {
-			const tokenTypeIndex = this.legend.tokenTypes.indexOf(name);
-
+			// TODO(skewb1k): replace current O(n) indexOf lookup with a trie for token strings.
+			// This will allow early exit and O(length-of-string) lookup.
+			const tokenTypeIndex = ctx.legend.tokenTypes.indexOf(name);
 			if (tokenTypeIndex === -1) {
-				continue;
+				throw new Error(`Token type "${name}" not found in legend.tokenTypes.`);
 			}
 
 			builder.push(
@@ -86,26 +98,17 @@ class VieSemanticTokensProvider implements vsc.DocumentSemanticTokensProvider {
 	}
 }
 
-function registerTokenProvider(
-	context: vsc.ExtensionContext,
-	parser: Parser,
-	language: Language,
-	legend: vsc.SemanticTokensLegend,
-	highlights: string,
-) {
-	context.subscriptions.push(
+export async function activate(extensionContext: vsc.ExtensionContext) {
+	const ctx = await initContext();
+
+	const parser = new Parser();
+	parser.setLanguage(ctx.vieLanguage);
+
+	extensionContext.subscriptions.push(
 		vsc.languages.registerDocumentSemanticTokensProvider(
 			{ language: "vie" },
-			new VieSemanticTokensProvider(parser, language, legend, highlights),
-			legend,
+			new VieSemanticTokensProvider(parser),
+			ctx.legend,
 		),
 	);
-}
-
-export async function activate(context: vsc.ExtensionContext) {
-	const { parser, language } = await initVieLanguage();
-	const legend = await initLegend();
-	const highlights = await getHighlightsScm();
-
-	registerTokenProvider(context, parser, language, legend, highlights);
 }
